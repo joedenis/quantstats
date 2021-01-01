fbc84c577e20db80673a53ab94483af792ba4d22
joedenis <joseph.denis@gmail.com>
2021-01-01 12:35:44 +0000

    modified the tearsheet to have a distribution of daily returns

--- samples/tester1.py
+++ samples/tester1.py
@@ -85,5 +85,5 @@ def praescire_all_time(filename="all_praescire.csv", title= "Praescire"):
 
 
 if __name__ == "__main__":
-	run(filename="PRAESCIRE20.csv")
+	run(filename="PRAESCIRE21.csv", title="Praescire ytd21")
 	# praescire_all_time()
